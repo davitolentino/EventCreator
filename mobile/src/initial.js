2bb37ede0871c82fbde3d6a0ed7d35d660edc59e
davitolentino <davidtolentino2012@hotmail.com>
2021-08-10 20:09:16 -0300

    fix:Consertado a criacao do mobile

--- mobile/src/initial.js
+++ mobile/src/initial.js
@@ -9,7 +9,7 @@ import facebook from './scenes/facebook1'
 import map from './scenes/map'
 import menu from './scenes/menu'
 import profile from './scenes/profile'
-import AR from './scenes/AR'
+// import AR from './scenes/AR'
 import myEvent from './scenes/myEvent';
 import event from './scenes/event'
 import MyTeam from './scenes/myteam'
@@ -24,7 +24,7 @@ export default function App() {
           <Scene key="auth" component={AuthScene} hideNavBar={true}></Scene>
           <Scene key="map" component={map} hideNavBar={true}></Scene>
           <Scene key="myteam" component={MyTeam} hideNavBar={true}></Scene>
-          <Scene key="AR" component={AR} hideNavBar={true}></Scene>
+          {/* <Scene key="AR" component={AR} hideNavBar={true}></Scene> */}
           <Scene key="profile" component={profile} hideNavBar={true}></Scene>
           <Scene key="menu" component={menu} hideNavBar={true}></Scene>
           <Scene key="myevent" component={myEvent} hideNavBar={true}></Scene>
